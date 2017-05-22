54b757885c031f92edd275aa023a0d467d647e12
Chris Luiz <chris@chrisluiz.io>
2017-05-22 16:14:34 -0600

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+Walking through "Programming computer vision with Python" by Jan Erik Solem
---
+++ imtools.py
@@ -0,0 +1,9 @@
+"""
+Collection of basic image processing functions.
+"""
+import os
+
+
+def get_imlist(path):
+    return [os.path.join(path, f) for f in os.listdir(path)
+            if f.endswith('jpg')]
