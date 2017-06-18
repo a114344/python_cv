0fc49829b6cc993b8b16d926ebcc526101c31434
Chris Luiz <chris@chrisluiz.io>
2017-06-18 13:51:04 -0400

    clusternode class done

--- hcluster.py
+++ hcluster.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+import numpy as np
 
 
 class ClusterNode(object):
@@ -24,4 +25,41 @@ class ClusterNode(object):
         """
         return (self.left.get_cluster_elements() +
                 self.right.get_cluster_elements())
-        
+
+    def get_height(self):
+        """Return the height of a node, height is sum
+
+           of each branch.
+        """
+        return (self.left.get_height() + self.right.get_height())
+
+    def get_depth(self):
+        """Return the depth of a node, depth is max of
+
+           each child plus own distance.
+        """
+        return np.max(self.left.get_depth(),
+                      self.right.get_depth() +
+                      self.distance)
+
+
+class ClusterLeafNode(object):
+    def __init__(self, vec, id):
+        self.vec = vec
+        self.id = id
+
+    def extract(self, dist):
+        return [self]
+
+    def get_cluster_elements(self):
+        return [self.id]
+
+    def get_height(self):
+        return 1
+
+    def get_depth(self):
+        return 0
+
+
+def L2dist(v1, v2):
+    
