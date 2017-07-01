bd425110c58f13b5eeaaff721457f3062255079b
Chris Luiz <chris@chrisluiz.io>
2017-07-01 17:42:29 -0400

    dsift

--- dsift.py
+++ dsift.py
@@ -2,6 +2,8 @@
 """
 
 from __future__ import print_function
+from PIL import Image
+import numpy as np
 import sift
 
 
@@ -12,15 +14,38 @@ def process_image_dsift(imagename, resultname, size=20, steps=10,
        and the save the results to file.
 
        INPUT:
-        imagename (string): Image file location.
+        imagename (string): Input image file location.
 
-        resultname (string):
+        resultname (string): Output image file location.
 
-        size ():
+        size (int): Feature size.
 
-        steps (int):
+        steps (int): Steps between locations.
 
-        force_orienation (bool): 
+        force_orientation (bool): If false all iamges are assumed oriented
+                                  upwards.
 
-        resize (tuple): REquired image size
+        resize (tuple): Required image size.
+
+       OUTPUT:
+        Dense sift representation of image.
     """
+
+    im = Image.open(imagename).convert('L')
+    if resize != None:
+        im = im.resize(resize)
+    m, n = im.size
+
+    if imagename[-3:,] != 'pgm':
+        # create a pgm file
+        im.save('tmp.pgm')
+        imagename = 'tmp.pgm'
+
+    # create frames and save to temporary file
+    scale = size / 3.0
+    x, y = np.meshgrid(range(steps, m, steps), range(steps, n, steps))
+    xx, yy = x.flatten(), y.flatten(),
+    frame = np.aray([xx, yy, scale * np.ones(xx.shape[0]),
+                    np.zeros(xx.shape[0])])
+    np.savetxt = ('tmp.frame', frame.T, fmt='%03.3f')
+    
