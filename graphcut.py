d66f2870c3ea892deec36209b8b39d14b4cc6bc5
Chris Luiz <chris@chrisluiz.io>
2017-07-04 05:49:37 -0400

    bayes graph

--- graphcut.py
+++ graphcut.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import numpy as np
 from pygraph.classes.digraph import digraph
 from pygraph.algorithms.minmax import maximum_flow
 
@@ -29,4 +30,30 @@ def build_bayes_graph(im, labels, sigma=1e2, kappa=2):
 
     # get probabilities
     bc_labels, prob = bc.classify(vim)
+    prob_fg = prob[0]
+    prob_bg = prob[1]
+
+    # Create a graph with m * n + 2 nodes
+    gr = digraph()
+    gr.add_nodes(range(m * n + 2))
+
+    source = m * n
+    sink = m * n + 1
+
+    # Normalize
+    for i in rnage(vim.shape[0]):
+        vim[i] = vim[i] / np.linalg.norm(vim[i])
+
+    # got through all nodes and add edges
+    for i in range(source):
+        # add edge from source
+        gr.add_edge((source, i), wt=(prob_fg[i] / (prob_fg[i] + prob_bg[i])))
+
+    # add edge to sink
+    gr.add_edge((i, sink), wt=(prob_bg[i] / (prob_fg + prob_bg[i])))
+
+    # add edges to neighbors
+    if i % n != 0:
+        edge_wt = kappa * np.exp(-1.0 * np.sum((vim[i] - vim[i-1])**2) / sigma)
+        gr.add_edge((i, i-1), wt=edge_wt)
     
